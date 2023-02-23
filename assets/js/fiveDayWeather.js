c7b88845e6fbf64f6bb6f20e373071314524cab9
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-02-23 00:07:30 -0500

    added functionality for the 5 day weather to be displayed after a fetch.  Still need to work on the current day and the user search api functionaity

---
+++ assets/js/fiveDayWeather.js
@@ -0,0 +1,106 @@
+//Assuming we already have the longitude and latitude
+
+function getfiveDayForcast() {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+  )
+    .then((response) => {
+      if (response.status >= 500) {
+        throw new Error("An unknown error occurred.");
+      } else if (response.status >= 400) {
+        throw new Error("The resource you requested could not be found.");
+      } else if (response.status > 200) {
+        throw new Error("An error occurred on the server.");
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+
+      //populateFiveDayObject
+      data.list.forEach((element) => {
+        const date = moment(element.dt_txt).format("L");
+        fiveDayFilteredDates.add(date);
+      });
+
+      // Convert filteredDates to an array of unique dates
+      const uniqueDates = Array.from(fiveDayFilteredDates);
+
+      // Initialize newObject with date and empty hour array
+      uniqueDates.forEach((date) => {
+        fiveDayObject.push({ date, hour: [] });
+      });
+
+      console.log(fiveDayObject);
+
+      // Populate the hourly array in fiveDayObject\
+
+      let dateArrayIndex = 0;
+
+      data.list.forEach((e) => {
+        const date = moment(e.dt_txt).format("L");
+        if (date !== uniqueDates[dateArrayIndex]) {
+          dateArrayIndex++;
+        } else {
+          fiveDayObject[dateArrayIndex].hour.push({
+            datetime: e.dt_txt,
+            temp: e.main.temp,
+            tempMin: e.main.temp_min,
+            tempMax: e.main.temp_max,
+            feelLike: e.main.feels_like,
+            icon: `http://openweathermap.org/img/wn/${e.weather[0].icon}@2x.png`,
+            desciription: e.weather[0].description,
+            wind: e.wind.speed,
+          });
+        }
+      });
+
+      //populateHourlyObjects(uniqueDates);
+      //Call the function to generate the daily weather cards
+      createDailyWeatherCards(fiveDayObject);
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+}
+
+
+
+
+
+
+function createDailyWeatherCards(weatherArray) {
+    const weatherCardContainer = document.getElementById("weatherCardContainer");
+    weatherArray.forEach((day) => {
+      if (day.hour.length === 0) {
+        console.log("no data");
+        return; // skip this iteration
+      }
+      const avgTemp = day.hour.reduce((sum, hour) => sum + hour.temp, 0) / day.hour.length;
+      const curDayData = day.hour[2];
+      const date = day.date;
+      const weatherIconUrl = curDayData.icon;
+  
+      const weatherCard = `
+        <div class="weatherCard">
+          <p>${date}</p>
+          <img class="weatherIcon" src="${weatherIconUrl}" alt="weatherIcon" />
+          <p class="alignRight">${curDayData.desciription.toUpperCase()}</p>
+          <p>Temp: ${Math.floor(avgTemp)} ℉</p>
+          <p>High: ${curDayData.tempMax}</p>
+          <p>Low: ${curDayData.tempMin}</p>
+          <p>Wind: ${curDayData.wind}</p>
+          <p>Feels Like: ${curDayData.feelLike}</p>
+        </div>
+      `;
+  
+      weatherCardContainer.innerHTML += weatherCard;
+    });
+  }
+  
+
+
+
+
+getfiveDayForcast();
---
+++ assets/js/sharedVariables.js
@@ -0,0 +1,5 @@
+let lat = 40.08;
+let lon = -74.20;
+const apiKey = "043390da5a76d81703ed4fe2131f4f11";
+const fiveDayObject = [];
+const fiveDayFilteredDates = new Set();
